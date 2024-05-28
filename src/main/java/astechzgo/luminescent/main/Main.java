4d1ea6ea9ed0e48636e6870c91ebf5b6362e1039
Ryan Larkin <ryan.larkin@uwaterloo.ca>
2024-05-28 19:44:03 -0400

    Fix logging order

--- src/main/java/astechzgo/luminescent/main/Main.java
+++ src/main/java/astechzgo/luminescent/main/Main.java
@@ -14,8 +14,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		LoggingUtils.configureLogger();
 		SystemUtils.doOSSetUp();
+		LoggingUtils.configureLogger();
 		SystemUtils.setUpDebug();
 		
 		new Main().Run();
